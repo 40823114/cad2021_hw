c942c002cc96abf25716c344603fd7577ab3f315
40823114 <40823114@gm.nfu.edu.tw>
2021-10-07 11:16:56 +0800

    site1

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'Home', 'text': '學號：40823114 \n 網站倉儲： https://github.com/40823114/cad2021_hw \n 網站連結： https://40823114.github.io/cad2021_hw/content/index.html', 'tags': '', 'url': 'Home.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '學號：40823114 \n 網站倉儲： https://github.com/40823114/cad2021_hw \n 網站連結： https://40823114.github.io/cad2021_hw/content/index.html', 'tags': '', 'url': 'About.html'}]};
